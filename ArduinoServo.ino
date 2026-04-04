5ebe64bf56d18a8342e4982615b965de592e9b93
SEED <hhs_sjtu@sjtu.edu.cn>
2026-04-04 11:23:08 +0800

    initial commit

---
+++ ArduinoServo.ino
@@ -0,0 +1,21 @@
+#include "include.h"
+
+int ps2_ok;
+void setup() 
+{
+	pinMode(LED,OUTPUT);
+	pinMode(BUZZER,OUTPUT);
+	pinMode(KEY,INPUT_PULLUP);
+
+	InitPWM();
+	InitTimer2();
+	InitUart1();
+  ps2_ok = InitPS2();
+	InitFlash();
+	InitMemory();
+}
+
+void loop() 
+{
+	TaskRun(ps2_ok); 
+}
